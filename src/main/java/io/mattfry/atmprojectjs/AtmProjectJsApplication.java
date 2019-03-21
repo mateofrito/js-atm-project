405e774830faa6c72e1c1f952bced447dbf36109
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-20 20:01:44 -0400

    grade eclipse

---
+++ src/main/java/io/mattfry/atmprojectjs/AtmProjectJsApplication.java
@@ -0,0 +1,13 @@
+package io.mattfry.atmprojectjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmProjectJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmProjectJsApplication.class, args);
+	}
+
+}
